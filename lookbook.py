14a8aebec92b218aacf40992979c1e053d1c91bb
Jaime-gc-00 <78904735+Jaime-gc-00@users.noreply.github.com>
2022-05-10 16:36:50 +0200

    Add files via upload

---
+++ lookbook.py
@@ -0,0 +1,13 @@
+import String
+from Flask import current_app, Flask, request, render_template
+import json
+import os
+import requests
+
+app = Flask(__name__)
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_DIR = os.path.join(APP_DIR, 'templates')
+
+def main():
+    current_app.template_folder = TEMPLATE_DIR
+    return render_template('lookbook.html')
